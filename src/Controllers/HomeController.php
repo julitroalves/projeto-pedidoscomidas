f97eae30347eab02aaf09ad3dce9ff56207f7e3a
Julio Alves <julitroalves@gmail.com>
2018-11-03 15:10:24 -0300

    Database PDO: adding support to

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -4,11 +4,24 @@ namespace PedidosComidas\Controllers;
 
 class HomeController {
 
+	public function loadUsers() {
+		$pdo = new \PDO("mysql:host=localhost;dbname=housecursos_pedidoscomidas", "root", "qwe123");
+
+		$users = $pdo->query("SELECT * FROM users");
+
+		return $users;
+	}
+
 	public function index($request, $response, $renderer) {
 		$name = $request->query->get('name', 'Julio Alves');
 
+		$users = $this->loadUsers();
+
+		var_dump($users);
+
 		$context = [
 			'title' => "Home Dinâmica 100% melhorada!",
+			'users' => $users,
 			'name' => $name,
 		];
 
